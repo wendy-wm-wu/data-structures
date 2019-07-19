2a4b1cac4a31cfc7092c02fbc6f5a6a617d4e410
Wendy Wu <Wendy@Wendys-MacBook-Air.local>
2019-07-18 23:44:50 -0700

    Completed linked lists and trees

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,61 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //If there are no items in LL: no head or tail
+    //If there is ONE item: head and tail point to the same thing
+    //IF there is more than one, head and tail now point to resepctive first and last items
+    
+    //Create an instance of the node class
+    var newNode = Node(value);
+    if(list.head === null || list.tail === null) {
+      list.tail = newNode; 
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+    //Make the tail now equal/point to the newNode
+    //Make the newNode point to null, indicating that it is the end
+    //of the list
+
   };
 
   list.removeHead = function() {
+      //if head is not empty, 
+      //change head to the item next to head 
+     var temp = list.head.value; 
+     list.head = list.head.next;
+     return temp;
+
+    //Change list.head to equal to the new coming head variable
   };
 
   list.contains = function(target) {
-  };
+    //while loop to check all the items and compare to target 
+    //return boolean if it exists 
+         // if not inside, return false; 
+    if(list.head.value === target) {
+      return true; 
+    } 
+    var node = list.head.next; 
+    if (node !== null) { 
+     while(node !== null) {  
+      if(node.value === target) {
+        return true; 
+      } else {
+        return false; 
+      }
+      node = node.next; 
+      } 
+    } else {
+    return false; 
+  }
+  }
+
 
   return list;
 };
+
 
 var Node = function(value) {
   var node = {};
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,44 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  //an array containing a number of subtrees 
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+	//sets the target as a 2node and adds that node as a child of the tree 
+	//set value as a node that adds the node as a child of the tree
+	//if no children, add child 
+	// value = this.value; 
+	var newTree = Tree(value);  
+	this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+	//takes an input and returns a boolean to reflect whether it can be found as the value of the target node or any descendent node
+	//iterate through children subtrees
+	//traverse through each element using recursion
+	//base case
+	//if node matches target
+	//return true
+	//else, return false 
+	var node = Tree(target);
+	var flag = false;
+
+	function recurse(node) {
+		if(node.value === target) {
+			flag = true; 
+		}
+		for (let i = 0; i < node.children.length; i++) {
+			recurse(node.children[i]);
+		}
+	} 
+	recurse(this);
+	return flag; 
 };
 
 
