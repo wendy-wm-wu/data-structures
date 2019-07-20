6a02b84dc5ce3d6f248d60121c37a05810b76535
Wendy Wu <Wendy@Wendys-MacBook-Air.local>
2019-07-20 16:59:16 -0700

    Completed Hash Table and Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,111 @@
 var BinarySearchTree = function(value) {
+	//psedoclassical implementation
+
+	var binaryTree = Object.create(binarySearchTreeMethods); 
+	binaryTree.value = value; 
+	binaryTree.left = null;
+	binaryTree.right = null;
+
+	return binaryTree; 
 };
+
+//each BST can only have 0, 1, or 2 children.. so maybe push 
+
+var binarySearchTreeMethods = {
+
+	//check value and use left or right functions above to help 
+	//if smaller, go left, until there's an opening
+	//if larger, go right, until there's an opening 
+	insert: function(value) {
+		var treeNode = BinarySearchTree(value);
+
+		function searchNode(node) {
+			if(value < node.value) {
+				if(node.left === null) {
+					node.left = treeNode; 
+				} else {
+					searchNode(node.left); 
+				}
+			} 
+			if(value > node.value) {
+				if(node.right === null) {
+					node.right = treeNode;
+				} else {
+					searchNode(node.right);
+				}
+			}
+		}		
+		searchNode(this);
+	}, 
+	
+	contains: function(value) {
+	//use recursion 
+	//check array to see if item exists in array 
+	//initialize flag as false 
+	//helper function 
+		//if value is less than root node
+			// go left 
+			//if item === value
+				//set flag to true
+				//else, recurse to left 
+		//if value is larger than root node
+			//go right
+			//if item === value
+				//set flag to true
+					//recurse to right 
+		//return flag
+
+		let flag = false; 
+
+		function searchNode(node) {
+			//check if node is null first so you can catch it in the beginning 
+			if(node === null) { //base case 
+				return flag; 
+			}
+			if(value === node.value) { //base case 
+				flag = true; 
+			}
+			if(value < node.value) {
+				searchNode(node.left); 
+			}
+			if(value > node.value) {
+				searchNode(node.right); 
+			}
+		}
+		searchNode(this); 
+		return flag;
+	},
+
+	depthFirstLog: function(cb) {
+		//helper function
+		//if node isn't null, apply recursion
+		//iterate through each node and apply callback 
+		function recurse(node) {
+			// if(node !== null) {
+				cb(node.value);
+			// } 
+			if(node.left !== null) {
+				recurse(node.left);
+			}
+			if(node.right !== null) {
+				recurse(node.right);
+			}
+		}
+	recurse(this);	
+	}
+
+}
+//instantiate a class treeNode for the tree learn
+
+
+
+
+
+
+
+
+
+
 
 
 /*
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,19 +2,62 @@
 
 var HashTable = function() {
   this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+  this._storage = LimitedArray(this._limit); //will be a nested array that holds key, value pairs in a tuple if there are collisions
+  // this.bucket = [];
+  // this.tuple = []; //[key, value]
+  //define index here using the hashcode
+  //getIndexBelowMaxForKey(str, max)
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //use .set to set the items in the limited with index and value 
+  var tuple = [k, v]; //[key, value]
+  var bucket = this._storage.get(index); //bucket will be an array at the index 
+  console.log(bucket);
+  
+  if(bucket) { //if there's a key, value pair already in existence at the index 
+  	for (let i = 0; i < bucket.length; i++) {  //bucket is an array of arrays (nested array) //iterate through nested array 
+  		if(bucket[i][0] === k) { //stating that each array in the bucket is a key value pair if it exists 
+  			bucket[i][1] = v; 
+  		} else {	//else, if doesn't exist, then we will push the tuple to the bucket 
+  			bucket.push(tuple);
+  		}
+  	}	
+  }	else {
+  	this._storage.set(index, [tuple]); //if there is no bucket(no key value pair at the index), we will set the index with the key, value pair 
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  for (let i = 0; i < bucket.length; i++) {
+  	if(bucket[i][0] === k) {
+  		return bucket[i][1]; 
+  	}
+  }
+
+  //use .get to get the item using the index 
+  //check the tuple to see if it's there .. use .each 
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //use the index to find the key value pair to remove
+  //iterate through the tuple to check as well 
+  var bucket = this._storage.get(index); 
+
+  if(bucket) {
+  	for (let i = 0; i < bucket.length; i++) {
+  		if(bucket[i][0] === k) {
+  			bucket.splice(i, 1); 
+  		}
+  	}
+  	delete this._storage[index]; 
+  }
+ 
 };
 
 
